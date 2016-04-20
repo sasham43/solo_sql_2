e24b41a9566488014f5e7f929b69df9f6af2c422
sasham43 <sashasemail@gmail.com>
2016-04-20 15:05:01 -0500

    Redid queries using aggregate functions

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -1,30 +1,33 @@
--- Get all customers and their addresses.
+-- 1. Get all customers and their addresses.
 SELECT * FROM customers JOIN addresses ON customers.id = addresses.customer_id;
--- Get all orders and their line items.
+-- 2. Get all orders and their line items.
 SELECT * FROM orders JOIN line_items ON orders.id = line_items.order_id;
--- Which warehouses have cheetos?
+-- 3. Which warehouses have cheetos?
 SELECT products.description, warehouse.warehouse
 FROM products
 JOIN warehouse_product ON products.id = warehouse_product.product_id
 JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
 WHERE products.description = 'cheetos';
--- Which warehouses have diet pepsi?
-SELECT products.description, warehouse.warehouse
+-- 4. Which warehouses have diet pepsi?
+SELECT products.description, string_agg(warehouse.warehouse, ', ')
 FROM products
 JOIN warehouse_product ON products.id = warehouse_product.product_id
 JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
-WHERE products.description = 'diet pepsi';
--- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
-SELECT *
+WHERE products.description = 'diet pepsi'
+GROUP BY products.description;
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT customers.first_name, sum(orders.total)
 FROM customers
 JOIN addresses ON customers.id = addresses.customer_id
-JOIN orders ON orders.address_id = addresses.id;
--- How many customers do we have?
-SELECT * FROM customers;
--- How many products do we carry?
-SELECT * FROM products;
--- What is the total available on-hand quantity of diet pepsi?
-SELECT products.description, warehouse_product.on_hand
+JOIN orders ON orders.address_id = addresses.id
+GROUP BY customers.first_name;
+-- 6. How many customers do we have?
+SELECT count(*) FROM customers;
+-- 7. How many products do we carry?
+SELECT count(*) FROM products;
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT products.description, sum(warehouse_product.on_hand)
 FROM products
 JOIN warehouse_product ON products.id = warehouse_product.product_id
-WHERE products.description = 'diet pepsi';
+WHERE products.description = 'diet pepsi'
+GROUP BY products.description;
